69f5baca58e1796b0d4a3d98d70ed2d06d572a92
smartThinkFaruk <smartthink.omarfaruk@gmail.com>
2024-10-01 13:42:26 +0600

    component locally import done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div>
+    <app-header />
     <p>{{ name }}</p>
     <p>{{ age }}</p>
+    <app-footer />
   </div>
 </template>
 
 <script>
+import AppHeader from './Components/Header_footer/AppHeader.vue'
+import AppFooter from './Components/Header_footer/AppFooter.vue'
 export default {
   data() {
     return {
       name: 'Faruk',
       age: 25
     }
+  },
+  components:{
+    AppHeader,
+    AppFooter
   }
 }
 </script>
